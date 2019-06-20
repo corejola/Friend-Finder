15dfe6402fa492d4d461fa7e4b47035b0be50d9e
Cedric Orejola <cedricorejola@gmail.com>
2019-06-19 18:00:44 -0700

    updated project files

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friends = [
+    {
+        name: "yoda",
+        photo: "Yoda.png",
+        score: [
+            3,
+            1,
+            2,
+            3,
+            4,
+            5,
+            4,
+            5,
+            3,
+            2
+        ]
+    },
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+
+];

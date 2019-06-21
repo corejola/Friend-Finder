295ab03e70fbcdb418c807ea6bbdb1bd0038c9e1
Cedric Orejola <cedricorejola@gmail.com>
2019-06-21 16:48:10 -0700

    apiRoutes is now working

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,6 +9,7 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
+    //default path will take you to the home page
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 // Routes
 // =============================================================
 
+require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 //data validation for Friend information, no item can be left blank, submit button on click will process the data valadation
